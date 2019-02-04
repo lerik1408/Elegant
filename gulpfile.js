91ed5dc174718d11bc4d3584122b7a0f79c814f9
ilya <popruzhuk.38@gmail.com>
2019-02-05 01:30:30 +0200

    Born v1.1

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp        = require('gulp'), // Подключаем Gulp
+    uglify      = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+    del         = require('del'), // Подключаем библиотеку для удаления файлов и папок
+    sass        = require('gulp-sass'); //Подключаем Sass пакет
+
+gulp.task('sass', function(){ // Создаем таск "sass"
+        return gulp.src('app/sass/main.sass') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+});
+gulp.task('scripts', function() {
+    return gulp.src([ // Берем все необходимые библиотеки
+        'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
+    ])
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
+});
+
+gulp.task('watch',function() {
+    gulp.watch('app/sass/**/*.sass', gulp.parallel('sass')); // Наблюдение за sass файлами
+    // Наблюдение за другими типами файлов
+});
+
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('build', gulp.series('clean', 'sass', 'scripts'), function() {
+    gulp.src('app/css/main.css')
+    .pipe(gulp.dest('dist/css'))
+
+    gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('dist/fonts'))
+
+    gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+    .pipe(gulp.dest('dist/js'))
+
+    gulp.src('app/*.html') // Переносим HTML в продакшен
+    .pipe(gulp.dest('dist'));
+
+});
