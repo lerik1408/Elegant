cb2db8f305549c23b2fcc6ab984c3d5389d47d69
ilya <popruzhuk.38@gmail.com>
2019-02-06 18:33:27 +0200

    start

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    $('#s')
-})
+    var a = $('#k');
+      $.ajax({
+        type: "GET",
+        url: "header.html",
+        cache:false,
+        // data: "data",
+        dataType: "html",
+        success: function (response) {
+          $('body').append(response); 
+        }
+      });
+  });
