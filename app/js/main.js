589ee58108fff5f0f163ff5b71314110262e0f1b
ilya <popruzhuk.38@gmail.com>
2019-02-07 00:52:47 +0200

    end of the first day

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
-    var a = $('#k');
-      $.ajax({
+    var a = $.ajax({
         type: "GET",
         url: "header.html",
         cache:false,
         // data: "data",
         dataType: "html",
         success: function (response) {
-          $('body').append(response); 
+          $('body').append(response);
+        }
+      });
+      var b = $.ajax({
+        type: "GET",
+        url: "aboutus.html",
+        cache:false,
+        // data: "data",
+        dataType: "html",
+        success: function (response) {
+          $('body').append(response);
         }
       });
   });
